e6e1a79c18feed874812dc70ab3c3082ca3673d5
Vishv08 <156137210+Vishv08@users.noreply.github.com>
2024-04-05 23:43:22 -0400

    adding the class shape
    adding the class shape

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -12,11 +12,23 @@ const height = (canvas.height = window.innerHeight);
 // function to generate random number
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
 }
 
 // function to generate random color
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Shape {
+
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+
 }
