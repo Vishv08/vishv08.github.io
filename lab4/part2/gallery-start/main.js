0f342e9f73a665b5cc69a6b3c6b986ae25f69999
Vishv08 <156137210+Vishv08@users.noreply.github.com>
2024-04-05 23:18:38 -0400

    adding the loop
    adding the loop

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -9,12 +9,25 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the alternative text for each image file */
 
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+    'pic1.jpg' : 'a human eye',
+    'pic2.jpg' : 'a wave',
+    'pic3.jpg' : 'Purple and white pansies',
+    'pic4.jpg' : 'Section of wall',
+    'pic5.jpg' : 'a Large moth'
+  }
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 
 /* Wiring up the Darken/Lighten button */
